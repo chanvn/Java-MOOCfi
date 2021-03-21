a823d15aefe2dc9850996d95b0a2722bde049eed
Chanvir <chanvir.nijjar@gmail.com>
2021-03-21 16:02:43 -0400

    Variables summary

--- part01-Part01_15.DifferentTypesOfInput/src/main/java/DifferentTypesOfInput.java
+++ part01-Part01_15.DifferentTypesOfInput/src/main/java/DifferentTypesOfInput.java
@@ -7,6 +7,17 @@ public class DifferentTypesOfInput {
         Scanner scan = new Scanner(System.in);
 
         // Write your program here
-
+        System.out.println("Give a string:");
+        String message = scan.nextLine();
+        System.out.println("Give an integer:");
+        int numberInt = Integer.valueOf(scan.nextLine());
+        System.out.println("Give a double:");
+        double numberDouble = Double.valueOf(scan.nextLine());
+        System.out.println("Give a boolean:");
+        boolean tOrf = Boolean.valueOf(scan.nextLine());
+        System.out.println("You gave the string "+message);
+        System.out.println("You gave the integer "+numberInt);
+        System.out.println("You gave the double "+numberDouble);
+        System.out.println("You gave the boolean "+tOrf);
     }
 }
