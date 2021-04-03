2714466d75ce3765ede0b6413f6440e5c0bec924
Chanvir <chanvir.nijjar@gmail.com>
2021-04-02 22:27:46 -0400

    Calculations...you get the point, man

--- part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
+++ part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
@@ -7,6 +7,17 @@ public class SimpleCalculator {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
-
+        System.out.println("Give the first number:");
+        int firstNum = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int secondNum = Integer.valueOf(scanner.nextLine());
+        int sum = firstNum+secondNum;
+        int sub = firstNum-secondNum;
+        int prod = firstNum*secondNum;
+        double quotient = 1.0*firstNum/secondNum;
+        System.out.println(firstNum+" + "+secondNum+" = "+sum);
+        System.out.println(firstNum+" - "+secondNum+" = "+sub);
+        System.out.println(firstNum+" * "+secondNum+" = "+prod);
+        System.out.println(firstNum+" / "+secondNum+" = "+quotient);
     }
 }
