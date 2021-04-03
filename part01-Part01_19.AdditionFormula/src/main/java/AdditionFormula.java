f555cf69317ed9b2f2b915ab8a3c6c863c678d34
Chanvir <chanvir.nijjar@gmail.com>
2021-04-02 21:16:48 -0400

    Calculations IV

--- part01-Part01_19.AdditionFormula/src/main/java/AdditionFormula.java
+++ part01-Part01_19.AdditionFormula/src/main/java/AdditionFormula.java
@@ -7,6 +7,11 @@ public class AdditionFormula {
         Scanner scanner = new Scanner(System.in);
 
         // write your program here
-
+        System.out.println("Give the first number:");
+        int firstNum = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int secondNum = Integer.valueOf(scanner.nextLine());
+        int sum = firstNum + secondNum;
+        System.out.println(firstNum+" + "+secondNum+" = "+sum);
     }
 }
